22e9883f92cebefc504746c7082d33fb1a852337
Victor Luder <victor.luder@pbl.ee.ethz.ch>
2026-07-06 16:29:51 +0200

    fixed spelling error and changed button to google forms

---
+++ _next/static/BFpw49HyJJeJ9HG9fQw1q/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/landing_page_cyclowatt_2//_next/:path+",
+        "destination": "/landing_page_cyclowatt_2/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
